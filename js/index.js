ebe683fb7b067ef515efb9b32134e7f832158d0e
bei76583848 <76583848@qq.com>
2019-03-20 13:20:46 +0800

    coffee v1.0

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@
 var appData = {
 	orderShow: false,//false首页 | true订单页
 	randomNum: 1,//详情页banner随机数
-	phoneVal: '76583848',
-	orderCondition: '',//筛选订单列表条件 admin管理员
+	phoneVal: '',//筛选订单列表条件 admin管理员
 	admin: '',//管理员账号
+	adaptation: false,
+	paymentPgTx1: false,//下单失败文本显示
 	
-	qrcodeImg: '',
+	qrcodeSrc: '',
 	paytimer: null,
 	preDt: null,//获取二维码参数
 	payStatus: false,//付款状态
@@ -109,10 +110,18 @@ var app = new Vue({
         },
 		
 		init: function() {
-			this.phoneVal = '';
+			this.phoneVal = this.orderShow ? this.phoneVal : '';
 			this.pullIdx = -1;
 			this.orderListIdx = -1;
-			detailItem = null;
+			this.detailItem = null;
+			this.paymentPgTx1 = false;
+			//清除二维码 & 定时器
+			if(this.qrcodeSrc) {
+				this.qrcodeSrc = '';
+				clearInterval(app.paytimer);
+				app.paytimer = null;
+				//取消美团订单
+			}
 		},
 		
 		//打开选择桌号页
@@ -122,7 +131,9 @@ var app = new Vue({
 		//关闭选择桌号页
 		setTabNum: function(id) {
 			this.tabNum = id;
-			this.page.chioseTab = false;
+			setTimeout(function() {
+				app.page.chioseTab = false;
+			}, 200)
 		},
 		//打开详情页
 		showDetails: function(item) {
@@ -146,12 +157,21 @@ var app = new Vue({
 					return false;
 				}
 			}
-			this.orderCondition = this.phoneVal;
 			app.ajaxLoading = true;
 			axios('/api/v1/orders')
 				.then(function(res) {
 					for(var i = 0, len = res.data.length; i < len; i++) {
-						res.data[i].priceStr = (Number(res.data[i].totalFee) / 100).toFixed(2);
+						if(res.data[i].productionStatus == 'CREATE') {
+							res.data[i].productionStatusTx = '等待中';
+						}else if(res.data[i].productionStatus == 'COFFEEING') {
+							res.data[i].productionStatusTx = '制作中';
+						}else if(res.data[i].productionStatus == 'FLOWING') {
+							res.data[i].productionStatusTx = '拉花中';
+						}else if(res.data[i].productionStatus == 'DELIVERYING') {
+							res.data[i].productionStatusTx = '配送中';
+						}else if(res.data[i].productionStatus == 'COMPLETE') {
+							res.data[i].productionStatusTx = '完成';
+						}
 						//time
 						var payTime = parseInt(res.data[i].payTime);
 						var timeStr = new Date(payTime).getFullYear() + '.' + 
@@ -162,7 +182,10 @@ var app = new Vue({
 						res.data[i].timeStr = timeStr;
 					}
 					app.orderList = res.data;
-					app.ajaxLoading = false;
+					
+					setTimeout(function() {
+						app.ajaxLoading = false;
+					}, 500);
 					app.toIndex(true);
 				})
 				.catch(function(res) {
@@ -170,18 +193,25 @@ var app = new Vue({
 					app.fadeAlert('网络开小差了');
 				})
 		},
+		//刷新列表
 		refreshOrder: function() {
-			this.phoneVal = this.orderCondition;
 			this.toOrder();
 		},
 		
 		//首页
 		toIndex: function(bool) {
+			this.orderShow = bool;
 			this.init();
 			this.close();
-			this.orderShow = bool;
 			window.scrollTo(0,0);
 			this.pageShow();
+		},
+		showPhonePop: function() {
+			if(this.orderShow) {
+				this.toOrder();
+			}else {
+				this.popShow('phonePop');
+			}
 		},
 		//再次制作弹窗
 		idxLTxBtn: function(idx) {
@@ -190,8 +220,16 @@ var app = new Vue({
 		},
 		//再次制作
 		setAgain: function() {
-			this.orderList[this.orderListIdx].type = 1;
-			this.addOrder(this.orderList[this.orderListIdx], function() {
+			var order = this.orderList[this.orderListIdx];
+			this.addcoffee({
+				type: 1,
+				coffeeId: order.coffeeId,
+				flowId: order.flowId,
+				tableNumber: order.tableNumber,
+				userPhone: order.userPhone,
+				status: "CREATE",
+				orderId: order.id
+			}, function() {
 				app.fadeAlert('开始制作', 3000);
 				app.close();
 			})
@@ -210,150 +248,69 @@ var app = new Vue({
 				this.fadeAlert('请选择桌号');
 				return false;
 			}
-			this.orderCondition = this.phoneVal;
-			this.phoneVal = '';
 			this.page.paymentPg = true;
-		},
-		//付款页返回首页
-		paytoIndex: function() {
-			//清除二维码 & 定时器
-			if(this.qrcodeImg) {
-				this.qrcodeImg = '';
-				clearInterval(app.paytimer);
-				app.paytimer = null;
-				//取消美团订单
-			}
-			this.toIndex();
-		},
-		//付款成功，查看订单
-		seeOrder: function() {
-			this.paytoIndex();
-			this.phoneVal = this.orderCondition;
-			this.toOrder();
 		},
 		//选择支付方式生成支付二维码
 		chiosePay: function(str) {
-			//生成美团支付必要信息
-			this.preDt = this.setPreDt(str);
-			if(!this.preDt) return;
-			//请求成功返回
-			var code = {
-				status: 'SUCCESS',//SUCCESS成功；FAIL失败
-				errMsg: '请求失败显示文本',//错误消息描述（status 为失败的时候有）
-				qrCode: 'img/qrcode.png'
+			this.qrcodeSrc = str;
+			this.preDt = {
+				type: 0,
+				coffeeId: Number(app.detailItem.id),//咖啡种类ID
+				flowId: app.latteList[app.pullIdx] ? app.latteList[app.pullIdx].id : '',//拉花种类ID，不拉花0
+				tableNumber: app.tabNum,//桌号ID
+				userPhone: app.phoneVal,
+				createTime: new Date().getTime().toString(),
+				payStatus: 'ORDER_SUCCESS',
+				payTime: new Date().getTime().toString(),
+				totalFee: app.detailItem.mon.toFixed(4),//支付金额
+				channel: app.qrcodeSrc,
 			};
-			if(code.status == 'SUCCESS') {
-				//请求成功
-				app.qrcodeImg = 'img/qrcode.png';
-				app.queryOrder();
-			}else if(code.status == 'FAIL') {
-				app.fadeAlert(code.errMsg);
-			}
 		},
-		setPreDt: function(str) {
-			function S4() {return (((1+Math.random())*0x10000)|0).toString(16).substring(1);}
-			function guid() {return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());}
-			var dt = {
-				outTradeNo: "youibot" + new Date().getTime() + guid(),//接入方订单号，由接入方生成，不能重复
-				totalFee: 1,//订单金额，单位为分
-				subject: 'coffee',//订单主题
-				body: '美式咖啡',//订单内容,不超过64个字符
-				channel: str,//支付渠道
-				tradeType: 'NATIVE',//交易类型
-				merchantId: 30205,//开放平台分配的商户id, 目前是 美团POI ID
-				appId: 30000,//接入方标识，由美团开放平台分配
-				sign: 123456,//验证签名
-				random: "youibot" + parseInt(Math.random() * 1000000),//随机数
-			}
-			return dt;
-		},
-		//生成二维码成功，等待付款
-		queryOrder: function() {
-			//请求的参数
-			var data = {
-				outTradeNo: app.preDt.outTradeNo,
-				merchantId: app.preDt.merchantId,
-				appId: app.preDt.appId,
-				sign: app.preDt.sign,
-				random: app.preDt.random,
-			}
-			this.paytimer = setInterval(request, 1000);
-			
-			function request() {
-				//返回的参数
-				var code = {
-					status: 'SUCCESS',//SUCCESS成功；FAIL失败
-					orderStatus: 'ORDER_SUCCESS',
-				}
-				if(code.status == 'FAIL') {
-					app.fadeAlert(code.errMsg);
-					return false;
-				}
-				/*
-				订单状态
-				'ORDER_NEW' ： 交易新建
-				'ORDER_FAILED'：交易失败
-				'ORDER_REVERSALING'：交易冲正中
-				'ORDER_REVERSAL'：交易冲正
-				'ORDER_SUCCESS': 交易成功
-				'ORDER_PART_REFUND'：交易半退
-				'ORDER_ALL_REFUND'： 交易全退
-				'ORDER_REFUNDING'：退款中
-				'ORDER_CLOSE'：交易失败
-				*/
-				switch (code.orderStatus) {
-					//交易成功，订单下发
-					case 'ORDER_SUCCESS':
-						app.addOrder({
-							outTradeNo: app.preDt.outTradeNo,
-							type: 0,
-							coffeeId: 1,//咖啡种类ID
-							flowId: 1,//拉花种类ID
-							tableNumber: 1,//桌号ID
-							userPhone: 13164761010,
-							createTime: new Date().getTime().toString(),
-							payStatus: code.orderStatus,
-							payTime: new Date().getTime().toString(),
-							totalFee: 0.0100,//支付金额
-							channel: app.preDt.channel,//支付渠道
-						})
-						
-						app.payStatus = true;
-						app.popShow('payinfo');
-						clearInterval(app.paytimer);
-						app.paytimer = null;
-						break
-					//交易失败
-					case 'ORDER_FAILED':
-						app.payStatus = false;
-						app.popShow('payinfo');
-						clearInterval(app.paytimer);
-						app.paytimer = null;
-						break
-					//交易失败
-					case 'ORDER_CLOSE':
-						app.payStatus = false;
-						app.popShow('payinfo');
-						clearInterval(app.paytimer);
-						app.paytimer = null;
-						break
-				}
-			}
-		},
-		//通知后台订单下发
-		addOrder: function(data, cb) {
-			axios.post('/api/v1/order', data)
+		//付款成功
+		paySucc: function() {
+			app.ajaxLoading = true;
+			//订单下发
+			axios.post('/api/v1/orders', this.preDt)
 				.then(function (res) {
 					if(res.status == 201) {
-						cb && cb();
+						app.addcoffee({
+							type: app.preDt.type,
+							coffeeId: app.preDt.coffeeId,
+							flowId: app.preDt.flowId,
+							tableNumber: app.preDt.tableNumber,
+							userPhone: app.preDt.userPhone,
+							status: "CREATE",
+							orderId: res.data.id
+						}, function() {
+							app.payStatus = true;
+							app.popShow('payinfo');
+						});
 					}else {
-						app.fadeAlert('下单失败，重新下单中');
-						app.addOrder(data, cb);
+						app.ajaxLoading = false;
+						app.paymentPgTx1 = true;
 					}
 				})
-				.catch(function (error) {
-					app.fadeAlert('下单失败，请联系工作人员');
+				.catch(function () {
+					app.ajaxLoading = false;
+					app.paymentPgTx1 = true;
 				});
+		},
+		//通知后台订单下发
+		addcoffee: function(data, cb) {
+			app.ajaxLoading = true;
+			axios.post('/api/v1/coffeeProductions', data)
+				.then(function (res) {
+					app.ajaxLoading = false;
+					res.status == 201 && cb && cb();
+				})
+				.catch(function () {
+					app.ajaxLoading = false;
+					app.paymentPgTx1 = true;
+				});
+		},
+		
+		setAdaptation: function() {
+			app.adaptation = (window.innerHeight / window.innerWidth) < (580 / 640) ? true : false;
 		},
 		
 		getIndex: function() {
@@ -366,13 +323,21 @@ var app = new Vue({
 				app.tabList = res.data.table;
 				app.latteList = res.data.latte;
 				app.admin = res.data.admin;
-			})
+				axios.defaults.baseURL = res.data.baseURL;
+			});
+			this.setAdaptation();
+			document.getElementById('phoneInput').onfocus = function(e) {
+				setTimeout(function() {
+					e.target.scrollIntoView(true);
+				}, 200)
+			}
 		},
     },
     mounted: function () {
 		this.$nextTick(function() {
 			FastClick.attach(document.body);
 			this.getIndex();
-		})
-    },
+		});
+		window.onresize = this.setAdaptation;
+    }
 });
